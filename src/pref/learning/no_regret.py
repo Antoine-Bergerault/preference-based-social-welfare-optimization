94eb6f76cb3f41afcfe516b1a1f5ad3bf911958d
Antoine Bergerault <antoine.bergerault@gmail.com>
2024-03-25 18:43:11 +0100

    fix hedge function signature

--- src/pref/learning/no_regret.py
+++ src/pref/learning/no_regret.py
@@ -1,5 +1,5 @@
 from pref.utils import define_learning_algorithm
 
 @define_learning_algorithm(["hedge", "mw", "multiplicative_weights"])
-def hedge(function):
+def hedge(function, input_dim):
     raise NotImplementedError()
