d1aada571abd4adb62d8b2b8db5ad1510ed77d1a
Antoine Bergerault <antoine.bergerault@gmail.com>
2024-04-11 12:36:45 +0200

    fix hedge: from bandit to full feedback

--- src/pref/learning/no_regret.py
+++ src/pref/learning/no_regret.py
@@ -15,23 +15,36 @@ def hedge(function, input_dim, max_iters=30, epsilon=0.1):
     
     Q = np.ones((input_dim, k))
     
+    # TODO: make sure this maps makes sense for the algorithm
+    # it tries to map an arbitrary real value to something useful for the weights update
+    def loss_map(x):
+        return np.exp(-np.exp(x / 2))
+    
     for t in range(max_iters + 1):
         exp_Q = np.exp(Q)
         policy = exp_Q / np.sum(exp_Q, axis=1, keepdims=True)
         
-        actions_index = np.apply_along_axis(
+        chosen_actions_indices = np.apply_along_axis(
             func1d=lambda p: Random.numpy_rng.choice(a=k, p=p),
             axis=1,
             arr=policy
         )
-
-        actions = min_value + delta * actions_index
         
-        # TODO: make sure this maps makes sense for the algorithm
-        # it tries to map an arbitrary real value to something useful for the weights update
-        output = np.exp(-np.exp(function(actions) / 2))
-        
+        # return after max_iters updates
         if t == max_iters:
-            return actions, output
+            chosen_actions = min_value + delta * chosen_actions_indices
+            output = loss_map(function(chosen_actions))
+            return chosen_actions, output
         
-        Q[np.arange(input_dim), actions_index] *= np.exp(-epsilon * output)
+        # repeat players and actions indices to cover all possibilities
+        players_indices = np.repeat(np.arange(input_dim), k)
+        actions_indices = np.tile(np.arange(k), input_dim)
+        
+        # all unilateral changes including chosen actions (i.e. trivial changes)
+        unilateral_changes = np.kron(chosen_actions_indices, np.ones((input_dim, 1))) * (1 - np.eye(input_dim)) # masked actions indices
+        unilateral_changes = np.kron(unilateral_changes, np.ones((k, 1))) + np.kron(np.eye(input_dim), np.arange(k)).T # add unilateral changes
+        unilateral_changes = min_value + delta * unilateral_changes
+        
+        outputs = loss_map(np.apply_along_axis(function, 1, unilateral_changes))
+        
+        Q[players_indices, actions_indices] *= np.exp(-epsilon * outputs)
